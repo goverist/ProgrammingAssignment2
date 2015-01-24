679aef4710294d415764ceaf92924f955297df2e
Goverist <goverist@users.noreply.github.com>
2015-01-24 23:04:55 +0100

    CacheMatrix
    Function cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+## MakeCacheMatrix works together with cachesolve
+## Check if the matrix asked by Cachesolve has been
+## calculated before. If
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 m <- NULL
+
+set  <- function(y) {
+  x <<- y
+  m <<- NULL
+  }
+get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list( set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
@@ -12,4 +22,13 @@ m <- NULL
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)){
+    message("getting cached inverse matrix")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
